0ba6db2c070069e7d7337ac9338fdd4028cc51f0
JustBobinAround <roberthenryjunkins@gmail.com>
2024-01-30 14:34:35 -0500

    added access to function

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ pub mod hotkeys_provider;
 pub mod types;
 pub mod use_hotkeys;
 
-pub use use_hotkeys::use_hotkeys;
+pub use use_hotkeys::{use_hotkeys, use_hotkeys_scoped};
 
 pub use hotkeys_provider::{use_hotkeys_context, HotkeysContext, HotkeysProvider};
