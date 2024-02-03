c47de55a561ada0966c86694a2968a6df49fc93e
JustBobinAround <roberthenryjunkins@gmail.com>
2024-02-03 11:58:38 -0500

    added prelude.rs and linked to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod hotkeys_provider;
 pub mod types;
 pub mod use_hotkeys;
 pub mod macros;
+pub mod prelude;
 
 pub use use_hotkeys::{use_hotkeys_scoped, use_hotkeys_ref_scoped};
 
---
+++ src/prelude.rs
@@ -0,0 +1,4 @@
+pub use crate::use_hotkeys::{use_hotkeys_scoped, use_hotkeys_ref_scoped};
+pub use crate::hotkeys_provider::{use_hotkeys_context, HotkeysContext, HotkeysProvider};
+pub use crate::{use_hotkeys, use_hotkeys_ref};
+
