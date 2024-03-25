4a753d65875f649e0b5c7f3642a9bfd28b6e252b
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-03-25 19:16:26 +0100

    Rename `hotkeys_provider` module as `context` (#88)

--- leptos_hotkeys/src/lib.rs
+++ leptos_hotkeys/src/lib.rs
@@ -1,8 +1,8 @@
-mod hotkeys_provider;
+mod context;
 mod macros;
 mod types;
 mod use_hotkeys;
 
-pub use hotkeys_provider::{provide_hotkeys_context, use_hotkeys_context, HotkeysContext};
+pub use context::{provide_hotkeys_context, use_hotkeys_context, HotkeysContext};
 pub use types::{Hotkey, KeyboardModifiers};
 pub use use_hotkeys::{use_hotkeys_ref_scoped, use_hotkeys_scoped};
